e566a81bf5bb3a650086ccf7b3803bcd8ef3f8ea
Sam Noh <samnoh21@outlook.com>
2019-11-01 14:54:36 +1300

    styles(notfoundpage): add image

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,34 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+import media from 'styles/media';
 
 const PageContainer = styled.div`
-    padding: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: #fcf9f4;
+    height: 100vh;
     text-align: center;
-    position: relative;
 
     h1 {
+        font-weight: 400;
         font-size: 40px;
+        margin-bottom: 30px;
+
+        ${media.tablet`
+            font-size: 28px;
+        `};
+    }
+
+    img {
+        width: 500px;
+        margin-bottom: 35px;
+        filter: blur(15);
+
+        ${media.tablet`
+            width: 280px;
+        `};
     }
 `;
 
 const Redirect = styled(Link)`
-    text-decoration: underline;
-    position: absolute;
-    top: 30px;
-    left: 30px;
+    font-weight: 300;
+    color: gray;
+    border: 1px solid gray;
+    padding: 10px 15px;
+    border-radius: 5px;
+    transition: all 0.3s ease-in;
 
-    &:hover {
-        color: gray;
+    @media (hover: hover) {
+        &:hover {
+            background-color: white;
+            border-color: black;
+            color: black;
+        }
     }
 `;
 
 const NotFoundPage = () => {
     return (
-        <PageContainer>
-            <h1>404 Not Found</h1>
-            <Redirect to="/">Back Home</Redirect>
-        </PageContainer>
+        <>
+            <Helmet>
+                <title>404 — Page Not Found</title>
+            </Helmet>
+            <PageContainer>
+                <h1>404 — Page Not Found</h1>
+                <img src="/404.png" alt="Page Not Found" />
+                <Redirect to="/">Go Back Home</Redirect>
+            </PageContainer>
+        </>
     );
 };
 
