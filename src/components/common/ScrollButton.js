69381ff1a95d7a7435511896f05ed1a52c242d9e
Sam Noh <samnoh21@outlook.com>
2019-11-01 16:01:48 +1300

    refactor(scrollbutton): create scroll buttons component

---
+++ src/components/common/ScrollButton.js
@@ -0,0 +1,78 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as UpChevron } from 'assets/up-chevron.svg';
+import { ReactComponent as DownChevron } from 'assets/down-chevron.svg';
+
+const ButtonContainer = styled.div`
+    width: 90px;
+    height: 35px;
+    background-color: #e5e5e5;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    opacity: 0.6;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    border-top-left-radius: 5px;
+    border-top: 1px solid rgba(0, 0, 0, 0.3);
+    border-left: 1px solid rgba(0, 0, 0, 0.3);
+
+    div {
+        cursor: pointer;
+        height: 100%;
+        width: 100%;
+        text-align: center;
+
+        @media (hover: hover) {
+            &:hover {
+                opacity: 0.3;
+            }
+        }
+    }
+
+    .chevron {
+        fill: rgba(0, 0, 0, 0.3);
+        width: 20px;
+        height: 100%;
+    }
+`;
+
+const ScrollButton = ({ up, down, isSmooth }) => {
+    const scrollTo = useCallback(
+        toBottom => {
+            window.scrollTo({
+                top: toBottom === 'bottom' ? document.body.scrollHeight : 0,
+                left: 0,
+                behavior: isSmooth ? 'smooth' : 'auto'
+            });
+        },
+        [isSmooth]
+    );
+
+    if (!up && !down) return null;
+
+    return (
+        <ButtonContainer>
+            {up && (
+                <div onClick={() => scrollTo()}>
+                    <UpChevron class="chevron"></UpChevron>
+                </div>
+            )}
+            {down && (
+                <div onClick={() => scrollTo('bottom')}>
+                    <DownChevron class="chevron"></DownChevron>
+                </div>
+            )}
+        </ButtonContainer>
+    );
+};
+
+ScrollButton.defaultProps = {
+    up: true,
+    down: true,
+    isSmooth: false
+};
+
+export default ScrollButton;
